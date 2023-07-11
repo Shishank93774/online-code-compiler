46cad3068eb2d693c0d62716449985582896bd18
Shishank <shishankrawat93774@gmail.com>
2023-07-11 16:09:17 +0530

    d

---
+++ src/constants/customStyles.js
@@ -0,0 +1,56 @@
+export const customStyles = {
+  control: (styles) => ({
+    ...styles,
+    width: "100%",
+    maxWidth: "15rem",
+    minWidth: "12rem",
+    borderRadius: "12px",
+    color: "#000",
+    fontSize: ".9rem",
+    lineHeight: "1.8rem",
+    backgroundColor: "aliceblue",
+    cursor: "pointer",
+    border: "2px solid #000000",
+    boxShadow: "5px 5px 0px 0px rgba(0,0,0);",
+    ":hover": {
+      border: "2px solid #000000",
+      boxShadow: "none",
+    },
+  }),
+  option: (styles) => {
+    return {
+      ...styles,
+      color: "#000",
+      fontSize: "0.8rem",
+      lineHeight: "1.75rem",
+      width: "100%",
+      background: "#fff",
+      ":hover": {
+        backgroundColor: "rgb(243 244 246)",
+        color: "#000",
+        cursor: "pointer",
+      },
+    };
+  },
+  menu: (styles) => {
+    return {
+      ...styles,
+      backgroundColor: "#fff",
+      maxWidth: "14rem",
+      minWidth: "12rem",
+      scrollbar: "none",
+      border: "2px solid #000000",
+      borderRadius: "5px",
+      boxShadow: "5px 5px 0px 0px rgba(0,0,0);",
+    };
+  },
+
+  placeholder: (defaultStyles) => {
+    return {
+      ...defaultStyles,
+      color: "#000",
+      fontSize: "0.8rem",
+      lineHeight: "1.75rem",
+    };
+  },
+};
